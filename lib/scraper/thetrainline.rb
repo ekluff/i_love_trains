0c4d91cb1019e0687e9997a18033f62de8e3117f
Evan Clough <clough.evan@gmail.com>
2025-11-16 11:58:55 +0100

    WIP Main execution class for app

---
+++ lib/scraper/thetrainline.rb
@@ -0,0 +1,7 @@
+module Scraper
+  class Thetrainline
+    def self.find(from:nil, to:nil, departure_at:nil)
+      "Heyoo, you wanna take a train from #{from} to #{to} at #{departure_at}"
+    end
+  end
+end
